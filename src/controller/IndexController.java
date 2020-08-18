aff2f321fee28cfeef1149a42b09a97b9600550f
Melo <guilhermeamelo.dev@gmail.com>
2020-08-18 17:41:11 -0300

    Iniciando projeto - Subindo para o Git

---
+++ src/controller/IndexController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable{
+
+	private static final long serialVersionUID = -2701110827897445334L;
+
+}
